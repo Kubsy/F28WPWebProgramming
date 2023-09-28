e8da5e96aeb315d46ac8226ba4430f8ed2d539fa
Jakub Bilinski <80340234+Kubsy@users.noreply.github.com>
2023-09-28 17:54:10 +0100

    add example javascript

---
+++ src/HTML /script.js
@@ -0,0 +1,10 @@
+document.body.style.height = '200pt';
+
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://cdnjs.cloudflare.com/ajax/libs/cookie-banner/1.2.2/cookiebanner.min.js';
+script.id = 'cookiebanner';
+script['data-height']   = '20px'; // non-standard attribute (may need to use 'setAttribute(..)'
+script['data-position'] = 'top';
+script['data-message']  = 'We use cookies!';
+document.head.appendChild(script); 
